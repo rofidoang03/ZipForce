543b815efadfcd79ff451faced75b5ff6eb10781
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2024-05-23 22:25:07 +0700

    Create instal.sh

---
+++ instal.sh
@@ -0,0 +1,7 @@
+apt-get update
+apt-get install python3
+apt-get install python3-pip
+apt install python3.11-venv
+python3 -m venv /root/ZipForce/modules
+source /root/ZipForce/modules
+pip3 install -r requirements.txt
