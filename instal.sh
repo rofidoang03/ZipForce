b769ef5c2db256346ec49e1dd1569fdac9e67275
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2024-05-23 22:30:29 +0700

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 apt-get update
 apt-get install python3
 apt-get install python3-pip
 apt install python3.11-venv
 python3 -m venv /root/ZipForce/modules
-source /root/ZipForce/modules
+source /root/ZipForce/modules/bin/activate
 pip3 install -r requirements.txt
