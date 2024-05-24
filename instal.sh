45a1be5ffb5a5bb9a7423b5235ea23fc63d401bb
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2024-05-24 21:19:46 +0700

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
 
-apt-get update
-apt-get install python3
-apt-get install python3-pip
-apt install python3.11-venv
-python3 -m venv /root/ZipForce/modules
-source /root/ZipForce/modules/bin/activate
-pip3 install -r requirements.txt
+function cek_root(){
+
+}
+
+function install_dependency(){
+        dependency_list=(
+        "python3"
+        "python3-pip"
+        "python3.11-venv"
+        )
+
+        for dependency in "${dependency_list[@]}"; do
+                apt-get install "${dependency}"
+        done
+}
+
+function install_requirements_python3(){
+        python3 -m venv ~/ZipForce/modules
+        source ~/ZipForce/modules/bin/activate
+        pip3 install -r ~/ZipForce/requirements.txt
+}
+
+function install_Zip_Force(){
+        while true; do
+                read -p "Apakah Anda ingin menginstal ZipForce [Y/n] " nanya
+                if [[ "${nanya}" == "y" ]] || [[ "${nanya}" == "Y" ]]; then
+                        cek_root
+                        cek_koneksi_internet
+                        install_dependency
+                        install_requirements_python3
+                        break
+                else
+                        echo "[x] Proses instalasi dibatalkan."
+                        exit 1
+                fi
+        done
+}
+
+install_ZipForce
