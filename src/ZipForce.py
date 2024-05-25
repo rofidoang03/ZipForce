dc90823c071efebc3c5f20f579bc66a62c030aa0
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2024-05-25 15:57:26 +0700

    Update ZipForce.py

--- src/ZipForce.py
+++ src/ZipForce.py
@@ -25,13 +25,13 @@ except ImportError:
         sys.exit(1)
 try:
         # Modul pyzipper digunakan  untuk mengompresi 
-        # dan mengekstrak file dan data menggunakan format ZIP
+        # dan mengekstrak file dan data menggunakan format ZIP.
         import pyzipper
 except ImportError:
         print("Error: Modul pyzipper belum diinstal. Mohon instal dengan menjalankan perintah 'pip3 install pyzipper'.")
         sys.exit(1)
 
-# Variabel untuk mengatur warna output teks menggunakan modul colorama
+# Variabel untuk mengatur warna output teks menggunakan modul colorama.
 h = colorama.Fore.LIGHTGREEN_EX  # Hijau terang 
 b = colorama.Fore.LIGHTBLUE_EX   # Biru terang 
 m = colorama.Fore.LIGHTRED_EX    # Merah terang 
@@ -41,12 +41,15 @@ k = colorama.Fore.LIGHTYELLOW_EX # Kuning terang
 r = colorama.Style.RESET_ALL     # Reset 
 
 def ekstrak_file_zip(file_zip, folder, file_wordlist):
+        # Membuka file wordlist dengan encoding latin-1.
         with open(file_wordlist, 'r', encoding="latin-1", errors="ignore") as wordlist:
                 print(f"\n{b}[*] {p}Mulai ekstraksi. Saat dimulai tekan [CTRL+C] untuk berhenti.{r}\n")
                 time.sleep(3)
+                # Membaca setiap kata sandi dari file wordlist.
                 for kata_sandi in wordlist:
                         kata_sandi = kata_sandi.strip()
                         try:
+                                # Membuka file zip dengan pyzipper dan mencoba kata sandi.
                                 with pyzipper.AESZipFile(file_zip) as fz:
                                         fz.pwd = kata_sandi.encode('latin-1')
                                         fz.extractall(path=folder)
@@ -61,14 +64,14 @@ def ekstrak_file_zip(file_zip, folder, file_wordlist):
 
 if __name__ == "__main__":       
 
-        # folder untuk menyimpan file hasil ekstraksi
+        # folder untuk menyimpan file hasil ekstraksi.
         folder = "Hasil Ekstraksi"
 
-        # Membuat folder 'Hasil Ekstraksi'
+        # Membuat folder 'Hasil Ekstraksi' jika belum ada.
         if not os.path.exists(folder):
                 os.makedirs(folder)
 
-        # Memasukkan nama file zip 
+        # Memasukkan nama file zip.
         while True:
                 file_zip = input(f"{k}[?] {p}Nama file zip: ")
                 if os.path.exists(file_zip):
@@ -80,7 +83,7 @@ if __name__ == "__main__":
                 else:
                         print(f"{m}[-] {p}File {file_zip} tidak ditemukan.{r}")
 
-        # Memasukkan nama file wordlist 
+        # Memasukkan nama file wordlist.
         while True:
                 file_wordlist = input(f"{k}[?] {p}Nama file wordlist: ")
                 if os.path.exists(file_wordlist):
@@ -89,4 +92,6 @@ if __name__ == "__main__":
                 else:
                         print(f"{m}[-] {p}File {file_wordlist} tidak ditemukan.{r}")
 
+        
+        # Menjalankan fungsi ekstrak_file_zip dengan parameter 'file_zip, folder, file_wordlist'.
         ekstrak_file_zip(file_zip, folder, file_wordlist)
